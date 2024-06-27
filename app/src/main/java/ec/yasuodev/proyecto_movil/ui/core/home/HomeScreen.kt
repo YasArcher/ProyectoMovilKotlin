98909d5c28a319c54f2b013ad7c90f62094fa4a2
Marlon Masabanda <mmasabanda5946@uta.edu.ec>
2024-06-26 22:17:03 -0500

    Aplicacion funcional antes de hacer modificaciones grandes

--- app/src/main/java/ec/yasuodev/proyecto_movil/ui/core/home/HomeScreen.kt
+++ app/src/main/java/ec/yasuodev/proyecto_movil/ui/core/home/HomeScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -141,4 +141,8 @@ fun DefaultCard(store: Store, navController: NavController, user: User) {
             }
         }
     }
+}
+
+@Composable
+fun SettingsScreen() {
 }
